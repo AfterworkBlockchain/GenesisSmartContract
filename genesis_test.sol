5ba0435f461334f9b96a31dba64a1d02705157e7
Cuiny <45130917+Cuiny@users.noreply.github.com>
2018-12-30 17:27:14 +0100

    Create genesis_test.sol

---
+++ genesis_test.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.0 <0.6.0;
+pragma experimental ABIEncoderV2;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+import "./genesis.sol";
+
+contract genesisTest {
+    GenesisSpace countrytotest;
+    
+    function beforeAll() public {
+        countrytotest = new GenesisSpace("Genesis space", "This is a genesis space country", 1000);
+    }
+    
+    //Test the accept function - accepting citizens
+    function checkAcceptingCitizen() public {
+        countrytotest.accept(address(1), GenesisSpace.Citizen("Anna", 10));
+        countrytotest.accept(address(2), GenesisSpace.Citizen("Bob", 20));
+        //test the citizen list
+        Assert.equal(countrytotest.getCitizenList().length, 2, "Length does not match!");
+        Assert.equal(countrytotest.getCitizenList(0), address(1), "Address does not match!");
+        //test the mapping of citizens
+        Assert.equal(countrytotest.getCitizen(address(1)).name, "Anna", "Anna should be the citizen name!");
+        Assert.equal(countrytotest.getCitizen(address(1)).balance, 10, "Anna: Balance does not match!");
+        Assert.equal(countrytotest.getCitizen(address(2)).name, "Bob", "Bob should be the citizen name!");
+        Assert.equal(countrytotest.getCitizen(address(2)).balance, 20, "Bob: Balance does not match!");
+    }
+    
+    //Test the remove function - removing citizens
+    fuction checkRemovingCitizen() public {
+        //TODO
+    }
+}
