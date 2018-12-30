283f1ede6e716d8b2fe18a0839a32b3804c472ad
Cuiny <45130917+Cuiny@users.noreply.github.com>
2018-12-30 18:24:00 +0100

    Update genesis.sol
    Added more fields for the country type.

--- genesis.sol
+++ genesis.sol
@@ -2,15 +2,19 @@ pragma solidity >0.4.0;
 pragma experimental ABIEncoderV2;
 
 contract GenesisSpace{
-    //define a genesis country.
+    //specify a genesis country.
     struct Country {
-        string name;
-        string description;
+        string name; 
+        string description; 
+        string programAddr; //address for mini programs
+        string programURL; //URL for mini programs
         uint treasury;
+        uint entryCost;
+        uint exitCost;
         mapping (address => Citizen) citizens;
     }
     
-    //define a citizen.
+    //specify a citizen.
     struct Citizen {
         string name;
         uint balance;
@@ -43,7 +47,7 @@ contract GenesisSpace{
         require(i != citizenList.length, "Citizen is not found!");
     }
     
-    //Remove a citizen.
+    //Removes a citizen.
     function remove(address citizenAddr) public returns (bool) {
         uint index = lookup(citizenAddr);
         if(index < citizenList.length) {
@@ -56,24 +60,83 @@ contract GenesisSpace{
         }
     }
     
-    //Get citizen address list.
+    //Gets citizen address list.
     function getCitizenList() public view returns (address[] memory) {
         return citizenList;
     }
     
-    //Get the address of the ith citizen from the citizen address list.
+    //Gets the address of the ith citizen from the citizen address list.
     function getCitizenList(uint i) public view returns (address) {
         return citizenList[i];
     }
     
-    //Get a citizen tuple based on the address.
+    //Gets a citizen tuple based on the address.
     function getCitizen(address addr) public view returns (Citizen memory) {
         return country.citizens[addr];
     }
     
-    //Get balance for a citizen.
+    //Gets balance for a citizen.
     function getCitizenBalance(address citizenAddr) public view returns (uint balance_) {
         balance_ = country.citizens[citizenAddr].balance;
     }
     
+    //get the country name.
+    function getName() public view returns (string memory) {
+        return country.name;
+    }
+    
+    //set the contry name (modifying name might involve voting).
+    //function setName(string memory name_) public {
+    //    country.name = name_;
+    //}
+    
+    //get the country description.
+    function getDescription() public view returns (string memory) {
+        return country.description;
+    }
+    
+    //set the contry description (modifying description might involve voting).
+    //function setDescription(string memory description_) public {
+    //    country.description = description_;
+    //}
+    
+    //get the mini program address.
+    function getProgramAddr() public view returns (string memory) {
+        return country.programAddr;
+    }
+    
+    //set the mini program address.
+    function setName(string memory programAddr_) public {
+        country.programAddr = programAddr_;
+    }
+    
+    //get the mini program URL.
+    function getProgramURL() public view returns (string memory) {
+        return country.programURL;
+    }
+    
+    //set the mini program URL.
+    function setProgramURL(string memory programURL_) public {
+        country.programURL = programURL_;
+    }
+    
+    //get the entry cost.
+    function getEntryCost() public view returns (uint) {
+        return country.entryCost;
+    }
+    
+    //set the entry cost.
+    function setEntryCost(uint cost_) public {
+        country.entryCost = cost_;
+    }
+    
+    //get the exit cost.
+    function getExitCost() public view returns (uint) {
+        return country.exitCost;
+    }
+    
+    //set the exit cost.
+    function setExitCost(uint cost_) public {
+        country.exitCost = cost_;
+    }
 }
