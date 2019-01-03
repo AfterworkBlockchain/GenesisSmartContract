e7620c3ba7aa22799407c62dd633a2f2b45e1f6b
Cuiny <45130917+Cuiny@users.noreply.github.com>
2019-01-03 22:37:29 +0100

    Update genesis.sol
    Modified the join and leave logic

--- genesis.sol
+++ genesis.sol
@@ -11,25 +11,25 @@ contract GenesisSpace{
         uint treasury;
         uint entryCost;
         uint exitCost;
-        mapping (address => Citizen) citizens;//TODO: not sure if we need this.
+        //mapping (address => Citizen) citizens;//TODO: not sure if we need this.
     }
     
-    //define a citizen. TODO: not sure if we need this.
-    struct Citizen {
-        string name;
-        uint balance; 
-    }
+    //define a citizen. TODO: not sure if we need this. For now, we simplify it.
+    //struct Citizen {
+    //    string name;
+    //    uint balance; 
+    //}
     
     Country country;
     address payable countryCreator;
     address[] citizenList;
+    mapping (address => uint) balances;
     
-    //create a country with name, description and treasury.
-    constructor(string memory name_, string memory description_, uint treasury_) public {
+    //create a country.
+    constructor(string memory name_, string memory description_) public {
         countryCreator = msg.sender;
         country.name = name_;
         country.description = description_;
-        country.treasury = treasury_;
     }
     
     modifier onlyCountry() {
@@ -49,12 +49,13 @@ contract GenesisSpace{
     }
     
     //join the country. It can be only called by citizens.
-    //TODO: do we need the name information of the sender??
-    function join(string memory name_) public onlyCitizen payable returns (bool) {
-        require(msg.value >= country.entryCost, "Fail to pay the entry cost!");
-        countryCreator.transfer(country.entryCost);
-        Citizen memory citizen = Citizen(name_, msg.sender.balance);
-        country.citizens[msg.sender] = citizen;//TODO:check whether the citizen already exists
+    function join() public onlyCitizen payable returns (bool) {
+        require(msg.value >= country.entryCost+country.exitCost, "The money sent must be larger than the sum of the entry and exit cost!");
+        countryCreator.transfer(country.entryCost); // the money is transferred to the address of the country creator.
+        country.treasury += country.entryCost; //update the treasury
+        balances[msg.sender] = msg.value - country.entryCost;
+        //Citizen memory citizen = Citizen(name_, msg.sender.balance);
+        //country.citizens[msg.sender] = citizen; //TODO:check whether the citizen already exists
         citizenList.push(msg.sender); //add the citizen address to the citizen list
         return true;
     }
@@ -70,13 +71,16 @@ contract GenesisSpace{
     
     //leave the country. 
     function leave() public onlyCitizen payable returns (bool) {
-        require(msg.value >= country.exitCost, "Fail to pay the exit cost!");
+        balances[msg.sender] += msg.value; 
+        require(balances[msg.sender] >= country.exitCost, "Fail to pay the exit cost!");
         countryCreator.transfer(country.exitCost);
+        balances[msg.sender] -= country.exitCost;
+        country.treasury += country.exitCost;
         uint index = lookup(msg.sender);
         if(index < citizenList.length) {
             citizenList[index] = citizenList[citizenList.length-1]; 
             citizenList.length--;
-            delete country.citizens[msg.sender];
+            //delete country.citizens[msg.sender];
             return true;
         } else {
             return false;
@@ -94,13 +98,18 @@ contract GenesisSpace{
     }
     
     //get the citizen name based on the citizen address.
-    function getCitizenName(address citizenAddr) public view returns (string memory) {
-        return country.citizens[citizenAddr].name;
-    }
+    //function getCitizenName(address citizenAddr) public view returns (string memory) {
+    //    return country.citizens[citizenAddr].name;
+    //}
     
     //get balance for a citizen.
-    function getCitizenBalance(address citizenAddr) public view returns (uint balance_) {
-        balance_ = country.citizens[citizenAddr].balance;
+    //function getCitizenBalance(address citizenAddr) public view returns (uint balance_) {
+    //    balance_ = country.citizens[citizenAddr].balance;
+    //}
+    
+    //get balance based on a citizen address.
+    function getBalance(address addr) public view returns (uint) {
+        return balances[addr];
     }
     
     //get the country name.
@@ -119,7 +128,7 @@ contract GenesisSpace{
     }
     
     //set the contry description. TODO: modifying name might involve voting.
-    function setDescription(string memory description_) public onlyCountry{
+    function setDescription(string memory description_) public onlyCountry {
         country.description = description_;
     }
     
@@ -161,5 +170,10 @@ contract GenesisSpace{
     //set the exit cost.
     function setExitCost(uint cost_) public onlyCountry {
         country.exitCost = cost_;
+    }
+    
+    //get the country treasury.
+    function getTreasury() public onlyCountry view returns (uint) {
+        return country.treasury;
     }
 }
